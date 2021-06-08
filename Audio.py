52cbc9cd62509563a40847bd21fff29e2bb5d5a0
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-08 18:14:29 +0530

    disable getvolume

--- Audio.py
+++ Audio.py
@@ -3,7 +3,8 @@ from subprocess import PIPE, run
 
 class Audio:
     def __init__(self):
-        self.volume = int(self.readVolume())
+        # self.volume = int(self.readVolume())
+        self.volume = 0
 
     def readVolume(self):
         result = run("awk -F\"[][]\" '/dB/ { print $2 }' <(amixer -M sget Headphone)", stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
