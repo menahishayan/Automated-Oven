7493339ef5e0d985833c61e92dbe100a71751791
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-10 17:48:22 +0530

    cooking bug fix

--- Cook.py
+++ Cook.py
@@ -43,7 +43,7 @@ class Cook:
             self.steps = (await self.db.get(item))['steps']
             self.totalSteps = len(self.steps)
 
-            for s in len(self.totalSteps):
+            for s in range(self.totalSteps):
                 self.currentStep = s
                 step = self.steps[s]
                 self.e.log(step)
