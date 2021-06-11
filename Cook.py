2a53c88e98f0fa91e750cb6e255809ba8b7321ac
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-11 16:45:14 +0530

    logging

--- Cook.py
+++ Cook.py
@@ -24,6 +24,8 @@ class Cook:
     async def cookingHandler(self):
         while not self.e._SIGKILL:
             self.e.log(self.isCooking)
+            self.e.log(len(self.steps))
+            self.e.log(self.currentStep)
             if not self.isCooking and len(self.steps) > 0 and self.currentStep == -1:
                 try:
                     self.isCooking = True
