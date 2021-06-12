502ad5a06687b94e45b09a7d603b311dbb511ceb
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-12 12:24:05 +0530

    scale

--- DisplayContent.py
+++ DisplayContent.py
@@ -88,8 +88,8 @@ class DisplayContent:
         image = image.crop((x, y, x + self.width, y + self.height))
 
         image = ImageEnhance.Color(image).enhance(3)
-        # image = ImageEnhance.Contrast(image).enhance(1.3)
-        image = ImageEnhance.Brightness(image).enhance(0.8)
+        image = ImageEnhance.Contrast(image).enhance(0.7)
+        # image = ImageEnhance.Brightness(image).enhance(0.6)
 
         image = invert(image)
         self.disp.image(image)
