0a0dde602f0db0f67bbd7e7509d0398d52a8e8c9
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-12 20:30:37 +0530

    cleanup

--- DisplayContent.py
+++ DisplayContent.py
@@ -298,11 +298,9 @@ class DisplayContent:
 
                     d = steps[curStepIndex]['endTime']-steps[curStepIndex]['startTime']
                     percent = round(n/d,2)
-                    self.e.log(percent)
-
-                    timeRemaining = int(steps[curStepIndex]['endTime'] - time())
-                    if timeRemaining < 0: 
+                    if percent > 1:
                         break
+                    timeRemaining = int(steps[curStepIndex]['endTime'] - time())
                     # pause
 
                 image.paste(
