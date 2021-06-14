35c94dd1a79fd9e94dbd571c5c8c45972cbbc75c
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-14 13:06:53 +0530

    WS close

--- EventHandler.py
+++ EventHandler.py
@@ -61,9 +61,10 @@ class EventHandler:
         self.logging.error(msg)
         self.logging.error(exc_info()[-1].tb_lineno)
         # self.logging.error(format_exc())
-        
+
     def sig_handler(self, _, _2):
         self._SIGKILL = True
+        self.server.close()
         exit()
 
     async def startWebsocket(self):
