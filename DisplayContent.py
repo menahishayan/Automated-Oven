b07cf9f4ee5f36b25413f41980e99a6f154afecc
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-14 15:08:18 +0530

    logging

--- DisplayContent.py
+++ DisplayContent.py
@@ -249,6 +249,7 @@ class DisplayContent:
         try:
             while not self.e._SIGKILL and not self.e.cook.SIGTERM and not self.e.cook.SIGPAUSE:
                 if steps[curStepIndex]['type'] == 'preheat' and steps[curStepIndex]['isDone']:
+                    self.e.log("Display: Preheat done")
                     break
                 image = Image.new("RGB", (self.width, self.height))
                 imDraw = ImageDraw.Draw(image)
@@ -282,6 +283,7 @@ class DisplayContent:
         try:
             while not self.e._SIGKILL and not self.e.cook.SIGTERM and not self.e.cook.SIGPAUSE:
                 if steps[curStepIndex]['type'] == 'cook' and steps[curStepIndex]['isDone']:
+                    self.e.log("Display: Cook done")
                     break
                 image = Image.new("RGB", (self.width, self.height))
                 imDraw = ImageDraw.Draw(image)
