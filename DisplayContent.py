a88bb387e27690dac15807c1d17ed14db36adbde
MenahiShayan <shayan1232001@yahoo.co.in>
2021-06-14 19:04:43 +0530

    endtime bug fix

--- DisplayContent.py
+++ DisplayContent.py
@@ -334,7 +334,7 @@ class DisplayContent:
         try:
             while not self.e._SIGKILL and not self.e.cook.SIGTERM and not self.e.cook.SIGPAUSE:
                 self.e.log("CenterScreen: Start {} End {}".format(time() - steps[curStepIndex]['startTime'], steps[curStepIndex]['endTime'] - time()))
-                if steps[curStepIndex]['endTime'] >= time():
+                if steps[curStepIndex]['endTime'] <= time():
                     break
                 image = Image.new("RGB", (self.width, self.height))
                 
@@ -354,7 +354,7 @@ class DisplayContent:
         try:
             while not self.e._SIGKILL and not self.e.cook.SIGTERM and not self.e.cook.SIGPAUSE:
                 self.e.log("CenterScreen: Start {} End {}".format(time() - steps[curStepIndex]['startTime'], steps[curStepIndex]['endTime'] - time()))
-                if steps[curStepIndex]['endTime'] >= time():
+                if steps[curStepIndex]['endTime'] <= time():
                     break
                 image = Image.new("RGB", (self.width, self.height))
                 
@@ -374,7 +374,7 @@ class DisplayContent:
         try:
             while not self.e._SIGKILL and not self.e.cook.SIGTERM:
                 self.e.log("CenterScreen: Start {} End {}".format(time() - steps[curStepIndex]['startTime'], steps[curStepIndex]['endTime'] - time()))
-                if steps[curStepIndex]['isDone']:
+                if steps[curStepIndex]['endTime'] <= time():
                     break
                 image = Image.new("RGB", (self.width, self.height))
                 
